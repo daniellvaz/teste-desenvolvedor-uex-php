faa659f74add505e627e68ec6ed64419ae873143
Daniel Vaz <daniellmvaz@gmail.com>
2026-05-31 14:16:27 -0300

    feat: create authentication flow

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,22 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\Private\LogoutController;
 use App\Http\Controllers\Api\Public\LoginController;
 use App\Http\Controllers\Api\Public\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterController::class);
+// Route::post('/reset-password', ResetPasswordController::class);
+// Route::post('/forgot-password', ForgotPasswordController::class);
+
 Route::post('/login', LoginController::class);
 
 // Route::post('/forgot-password', ForgotPasswordController::class);
 // Route::post('/reset-password', ResetPasswordController::class);
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
 
-//     Route::apiResource('contacts', ContactController::class);
+    // Route::apiResource('contacts', ContactController::class);
 
-//     Route::get('/address/search', AddressController::class);
+    // Route::get('/address/search', AddressController::class);
 
-//     Route::delete('/account', DeleteAccountController::class);
+    // Route::delete('/account', DeleteAccountController::class);
 
-//     Route::post('/logout', LogoutController::class);
-// });
+    Route::delete('/logout', LogoutController::class);
+});
